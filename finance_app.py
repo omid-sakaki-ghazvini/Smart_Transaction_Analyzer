aecc892ac72cb90dcde6ee042ba6d176e9d7eb0d
omid sakaki ghazvini <103527073+omid-sakaki-ghazvini@users.noreply.github.com>
2025-06-15 19:34:57 +0330

    Update finance_app.py

--- finance_app.py
+++ finance_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import duckdb
 from datetime import datetime
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
-import torch
 
 # --- تنظیمات اولیه ---
 st.set_page_config(
